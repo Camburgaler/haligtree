a922f4e52eabd721791a357efbe2202d1b05914b
Cameron Chrobocinski <cameronchrobo@gmail.com>
2024-09-20 08:21:22 -0500

    added constants

--- src/app/util/constants.tsx
+++ src/app/util/constants.tsx
@@ -1,3 +1,49 @@
+import Armor from "./types/armor";
+import Correction from "./types/correction";
+import Infusion from "./types/infusion";
+import Weapon from "./types/weapon";
+
+export const WEAPONS: Weapon[] = Object.values(require("../data/weapons.json"));
+export const INFUSIONS: Infusion = require("../data/infusions.json");
+export const CORRECTIONS: Correction[] = Object.values(
+    require("../data/damage.json")
+);
+export const HELMETS: Armor[] = Object.values(require("../data/helmets.json"));
+export const CHESTPIECES: Armor[] = Object.values(
+    require("../data/chestpieces.json")
+);
+export const GAUNTLETS: Armor[] = Object.values(
+    require("../data/gauntlets.json")
+);
+export const LEGGINGS: Armor[] = Object.values(
+    require("../data/leggings.json")
+);
+
+export const ARMOR_RESULTS_SET_IDS = [
+    "first",
+    "second",
+    "third",
+    "fourth",
+    "fifth",
+];
+export const DAMAGE_TYPE_MODE_ANY: string = "any";
+export const DAMAGE_TYPE_MODE_All: string = "all";
+export const DAMAGE_TYPE_MODE_EXACTLY: string = "exactly";
+
+export const DAMAGE_TYPE_IDS: string[] = [
+    "physical",
+    "magic",
+    "fire",
+    "lightning",
+    "holy",
+    "blood",
+    "poison",
+    "frost",
+    "scarlet-rot",
+    "madness",
+    "sleep",
+];
+
 export const DAMAGE_TYPE_NAMES: string[] = [
     "Physical",
     "Magic",
@@ -10,4 +56,92 @@ export const DAMAGE_TYPE_NAMES: string[] = [
     "Scarlet Rot",
     "Madness",
     "Sleep",
+];
+
+export const DAMAGE_TYPE_ID_TO_NAME: { [key: string]: string } = {
+    [DAMAGE_TYPE_IDS[0]]: DAMAGE_TYPE_NAMES[0],
+    [DAMAGE_TYPE_IDS[1]]: DAMAGE_TYPE_NAMES[1],
+    [DAMAGE_TYPE_IDS[2]]: DAMAGE_TYPE_NAMES[2],
+    [DAMAGE_TYPE_IDS[3]]: DAMAGE_TYPE_NAMES[3],
+    [DAMAGE_TYPE_IDS[4]]: DAMAGE_TYPE_NAMES[4],
+    [DAMAGE_TYPE_IDS[5]]: DAMAGE_TYPE_NAMES[5],
+    [DAMAGE_TYPE_IDS[6]]: DAMAGE_TYPE_NAMES[6],
+    [DAMAGE_TYPE_IDS[7]]: DAMAGE_TYPE_NAMES[7],
+    [DAMAGE_TYPE_IDS[8]]: DAMAGE_TYPE_NAMES[8],
+    [DAMAGE_TYPE_IDS[9]]: DAMAGE_TYPE_NAMES[9],
+    [DAMAGE_TYPE_IDS[10]]: DAMAGE_TYPE_NAMES[10],
+};
+
+export const INFUSION_IDS: string[] = Object.keys(INFUSIONS);
+
+export const INFUSION_NAMES: string[] = [
+    "Standard",
+    "Heavy",
+    "Keen",
+    "Quality",
+    "Magic",
+    "Fire",
+    "Flame Art",
+    "Lightning",
+    "Sacred",
+    "Poison",
+    "Blood",
+    "Cold",
+    "Occult",
+];
+
+export const INFUSION_ID_TO_NAME: { [key: string]: string } = {
+    [INFUSION_IDS[0]]: INFUSION_NAMES[0],
+    [INFUSION_IDS[1]]: INFUSION_NAMES[1],
+    [INFUSION_IDS[2]]: INFUSION_NAMES[2],
+    [INFUSION_IDS[3]]: INFUSION_NAMES[3],
+    [INFUSION_IDS[4]]: INFUSION_NAMES[4],
+    [INFUSION_IDS[5]]: INFUSION_NAMES[5],
+    [INFUSION_IDS[6]]: INFUSION_NAMES[6],
+    [INFUSION_IDS[7]]: INFUSION_NAMES[7],
+    [INFUSION_IDS[8]]: INFUSION_NAMES[8],
+    [INFUSION_IDS[9]]: INFUSION_NAMES[9],
+    [INFUSION_IDS[10]]: INFUSION_NAMES[10],
+    [INFUSION_IDS[11]]: INFUSION_NAMES[11],
+    [INFUSION_IDS[12]]: INFUSION_NAMES[12],
+};
+
+export const CATEGORY_NAMES: string[][] = [
+    [
+        "Daggers",
+        "Straight Swords",
+        "Greatswords",
+        "Colossal Swords",
+        "Thrusting Swords",
+        "Heavy Thrusting Swords",
+        "Curved Swords",
+        "Curved Greatswords",
+        "Katanas",
+        "Twinblades",
+        "Hammers",
+        "Great Hammers",
+        "Flails",
+        "Axes",
+        "Greataxes",
+        "Spears",
+        "Great Spears",
+        "Halberds",
+        "Scythes",
+        "Whips",
+        "Fists",
+        "Claws",
+        "Colossal Weapons",
+        "Torches",
+        "Thrusting Shield",
+        "Hand-to-Hand Arts",
+        "Throwing Blades",
+        "Backhand Blades",
+        "Perfume Bottles",
+        "Beast Claws",
+        "Light Greatsword",
+        "Great Katana",
+    ],
+    ["Light Bows", "Bows", "Greatbows", "Crossbows", "Ballistae"],
+    ["Small Shields", "Medium Shields", "Greatshields"],
+    ["Glintstone Staves", "Sacred Seals"],
 ];
