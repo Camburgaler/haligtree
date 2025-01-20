aa9a1a19d07e08c983ca3c0ac8f97be09cbbbf9c
Cameron Chrobocinski <cameronchrobo@gmail.com>
2025-01-20 10:29:34 -0600

    added ability to remember armor page data between sessions

---
+++ src/app/util/types/armorSet.tsx
@@ -0,0 +1,12 @@
+import Armor from "./armor";
+
+type ArmorSet = {
+    helmet?: Armor;
+    chestpiece?: Armor;
+    gauntlets?: Armor;
+    leggings?: Armor;
+    fitness?: number;
+    weight?: number;
+};
+
+export default ArmorSet;
--- src/app/util/types/set.tsx
+++
@@ -1,8 +0,0 @@
-import Armor from "./armor";
-
-type Set = Armor[] & {
-    fitness?: number;
-    weight?: number;
-};
-
-export default Set;
