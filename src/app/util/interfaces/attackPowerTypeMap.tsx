135e9f2b747e7ea6713cfb03cb6a89d9114772d6
Cameron Chrobocinski <cameronchrobo@gmail.com>
2025-01-23 08:08:09 -0600

    removed unnecessary comments from attackPowerTypeMap

--- src/app/util/interfaces/attackPowerTypeMap.tsx
+++ src/app/util/interfaces/attackPowerTypeMap.tsx
@@ -3,59 +3,15 @@
  */
 export default interface AttackPowerTypeMap<T> {
     [key: string]: T | undefined;
-
-    /**
-     * Physical attack power; required
-     */
     physical: T;
-
-    /**
-     * Magic attack power; required
-     */
     magic: T;
-
-    /**
-     * Fire attack power; required
-     */
     fire: T;
-
-    /**
-     * Lightning attack power; required
-     */
     lightning: T;
-
-    /**
-     * Holy attack power; required
-     */
     holy: T;
-
-    /**
-     * Blood loss status effect; optional
-     */
     bleed?: T;
-
-    /**
-     * Poison status effect; optional
-     */
     poison?: T;
-
-    /**
-     * Frost status effect; optional
-     */
     frost?: T;
-
-    /**
-     * Scarlet rot status effect; optional
-     */
     "scarlet-rot"?: T;
-
-    /**
-     * Madness status effect; optional
-     */
     madness?: T;
-
-    /**
-     * Frost status effect; optional
-     */
     sleep?: T;
 }
