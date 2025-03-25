ebbe76587f9df9fd36ffbcccb3e8fc79eb9ddddb
Cameron Chrobocinski <cameronchrobo@gmail.com>
2025-03-25 16:40:23 -0500

    reverted middleware

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async headers() {
+        return [
+            {
+                source: "/:path",
+                headers: [
+                    {
+                        key: "Content-Security-Policy",
+                        value: "script-src 'sha256-{HASHED_INLINE_SCRIPT}' 'strict-dynamic';object-src 'none';base-uri 'none'",
+                    },
+                ],
+            },
+        ];
+    },
+};
 
 export default nextConfig;
